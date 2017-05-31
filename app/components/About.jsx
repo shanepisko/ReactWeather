c38399c16a3103c52ff55fc4ec1cf7e6f534f7bf
Shane Pisko <spisko@maymediagroup.com>
2017-05-31 13:42:26 -0400

    style about page and link to resources. Style examples page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,16 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to the about page</p>
+      <ul>
+        <li>
+          <a href="http://facebook.github.io/react">React</a> - this is the framework used to build this app
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - this is what we use to search the weather
+        </li>
+      </ul>
     </div>
   );
 };
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-    <h3>Examples</h3>
+    <h1 className="text-center">Examples</h1>
     <p>
-      Welcome to examples
+      Here are a few example location to try out
     </p>
+    <ol>
+      <li>
+        <Link to="/?location=Runnemede">Runnemede, NJ</Link>
+      </li>
+      <li>
+        <Link to="/?location=Rio">Rio, Brazil</Link>
+      </li>
+    </ol>
   </div>
   );
 };
