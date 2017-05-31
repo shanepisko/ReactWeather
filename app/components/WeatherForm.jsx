f5e85214dee21ffbd3b057d0f367e61eac800e18
Shane Pisko <spisko@maymediagroup.com>
2017-05-31 14:37:34 -0400

    added custom styles and page title styles

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -14,7 +14,7 @@ var WeatherForm = React.createClass({
   render: function () {
     return (
       <form onSubmit={this.onFormSubmit}>
-        <input type="text" ref="location"/>
+        <input type="search" ref="location" placeholder="search weather by city"/>
         <button className="button expanded hollow">Get Weather</button>
       </form>
     );
